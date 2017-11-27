bc1db0a19a3a98810c565debb89b865dd33e6e75
waseem.suleiman <waseem.suleiman@WaseemS-LT.experitest.local>
2017-11-27 04:38:59 +0200

    Eribank

--- WaseemReporter/src/test/java/com/waseem/tests/SampleTest.java
+++ WaseemReporter/src/test/java/com/waseem/tests/SampleTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Parameters;
 public class SampleTest {
 	@Test
 	@Parameters({ "build", "accessKey" })
-	public void testMethod(String build,String accessKey) {
+	public void testMethod(String build, String accessKey) {
 		System.err.println("job that was passed in via Jenkins job " + build);
 		System.out.println("accessKey=" + accessKey);
 	}
